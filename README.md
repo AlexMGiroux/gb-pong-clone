0330e7600d8d6f3d4a21f39b1ce330974e3ecdc7
AMGirouxMtA <116688226+AMGirouxMtA@users.noreply.github.com>
2023-11-04 17:01:03 -0300

    Updated file names and README

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # gb-pong-clone
 Pong game re-created for Nintendo Gameboy using GBDK <br />
 Run 'make.bat' to compile (a copy of GBDK must exist in C:/) <br />
-Compilation produces a 'main.gb', which can be run on a Nintendo Game Boy emulator <br />
+Compilation produces a 'gb-pong.gb', which can be run on a Nintendo Game Boy emulator <br />
+OR just download 'gb-pong.gb' and try it on a Gameboy emulator<br />
 
 ![Model](pong-screenshot.png)
---
+++ gb-pong.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <gb/gb.h>
+
+unsigned char paddle[] =
+{
+  0xFF,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0x93,0xFF,
+  0xA5,0xFF,0xC9,0xFF,0x93,0xFF,0xA5,0xFF,
+  0xC9,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0x93,0xFF,
+  0xA5,0xFF,0xC9,0xFF,0x93,0xFF,0xA5,0xFF,
+  0xC9,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0xFF,0xFF
+};
+
+unsigned char paddle2[] =
+{
+  0xFF,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0x93,0xFF,
+  0xA5,0xFF,0xC9,0xFF,0x93,0xFF,0xA5,0xFF,
+  0xC9,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0x93,0xFF,
+  0xA5,0xFF,0xC9,0xFF,0x93,0xFF,0xA5,0xFF,
+  0xC9,0xFF,0x93,0xFF,0xA5,0xFF,0xC9,0xFF,
+  0x93,0xFF,0xA5,0xFF,0xC9,0xFF,0xFF,0xFF
+};
+
+unsigned char ball[] =
+{
+  0x00,0x00,0x3C,0x3C,0x7E,0x7E,0x7E,0x7E,
+  0x7E,0x7E,0x7E,0x7E,0x3C,0x3C,0x00,0x00
+};
+
+unsigned char clear[] =
+{
+  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+};
+
+void main()
+{    
+    NR52_REG = 0x80;
+    NR50_REG = 0x77;
+    NR51_REG = 0xFF;
+    
+    int yourScore = 0;
+    int oppScore = 0;
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf(" %d               %d\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", yourScore, oppScore);
+
+    bool ballRight = TRUE;
+    bool ballUp = TRUE;
+    
+    int x = 15;
+    int y = 75;
+
+    int oppx = 150;
+    int oppy = 75;
+
+    int ballx = 85;
+    int bally = 75;
+
+    SPRITES_8x16;
+    set_sprite_data(0, 8, paddle);
+    set_sprite_tile(0, 0);
+    move_sprite(0, x, y);
+    set_sprite_tile(1, 6);
+    move_sprite(1, x, y+16);
+
+    set_sprite_tile(2, 0);
+    move_sprite(2, oppx, oppy);
+    set_sprite_tile(3, 6);
+    move_sprite(3, oppx, oppy+16);
+
+    set_sprite_data(10, 1, ball);
+    set_sprite_data(0x0B, 1, clear);
+    set_sprite_tile(4, 0x0A);
+
+    move_sprite(4, ballx, bally);
+    SHOW_SPRITES;
+
+    while(1)
+    {
+        if (ballRight)
+        {
+            ballx++;
+            move_sprite(4, ballx, bally);
+        }
+        else
+        {
+            ballx--;
+            move_sprite(4, ballx, bally);
+        }
+
+        if (ballUp)
+        {
+            bally -= 2;
+            move_sprite(4, ballx, bally);
+        }
+        else
+        {
+            bally += 2;
+            move_sprite(4, ballx, bally);
+        }
+
+        if(ballx > oppx - 6)
+        {
+            if(bally < oppy + 30 && bally > oppy - 8)
+            {
+                ballRight = FALSE;
+
+                NR10_REG = 0x7E;
+                NR11_REG = 0xC1;
+                NR12_REG = 0x66;
+                NR13_REG = 0x7C;
+                NR14_REG = 0xC6;
+            }
+            else
+            {
+                ballx = 85;
+                bally = 75;
+                yourScore++;
+                ballRight = !ballRight;
+
+                int i;
+                for(i = 0; i < 5; i++)
+                {
+                    NR21_REG = 0x81;
+                    NR22_REG = 0xA6;
+                    NR23_REG = 0x37;
+                    NR24_REG = 0x87;
+                    delay(180);
+                }
+
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                printf(" %d               %d\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", yourScore, oppScore);
+            }
+        }
+        else if(ballx < x + 6)
+        {
+            if(bally < y + 30 && bally > y - 8)
+            {
+                ballRight = TRUE;
+
+                NR10_REG = 0x7E;
+                NR11_REG = 0xC1;
+                NR12_REG = 0x66;
+                NR13_REG = 0x7C;
+                NR14_REG = 0xC6;
+            }
+            else
+            {
+                ballx = 85;
+                bally = 75;
+                oppScore++;
+                ballRight = !ballRight;
+
+                int i;
+                for(i = 0; i < 5; i++)
+                {
+                    NR21_REG = 0x81;
+                    NR22_REG = 0xA6;
+                    NR23_REG = 0x37;
+                    NR24_REG = 0x87;
+                    delay(180);
+                }
+                
+                printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                printf(" %d               %d\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", yourScore, oppScore);
+            }
+        }
+
+        if(bally < 20)
+        {
+            ballUp = FALSE;
+        }
+        else if(bally > 140)
+        {
+            ballUp = TRUE;
+        }
+
+        if (bally > oppy)
+        {
+            if (ballx > (95))
+            {
+                oppy++;
+                move_sprite(2, oppx, oppy);
+                move_sprite(3, oppx, oppy+16);
+            }
+
+            else
+            {
+                 if (oppy < 75)
+                 {
+                     oppy++;
+                 }
+                
+                 if (oppy > 75)
+                 {
+                     oppy--;
+                 }
+
+                move_sprite(2, oppx, oppy);
+                move_sprite(3, oppx, oppy+16);
+            }
+        }
+        else if (bally < oppy)
+        {
+            if (ballx > (95))
+            {
+                oppy--;
+                move_sprite(2, oppx, oppy);
+                move_sprite(3, oppx, oppy+16);
+            }
+
+            else
+            {
+                if (oppy < 75)
+                 {
+                     oppy++;
+                 }
+                
+                 if (oppy > 75)
+                 {
+                     oppy--;
+                 }
+
+                move_sprite(2, oppx, oppy);
+                move_sprite(3, oppx, oppy+16);
+            }
+        }
+        
+        if(joypad() == J_UP && y > 20)
+        {
+            y--;
+            y--;
+            move_sprite(0, x, y);
+            move_sprite(1, x, y+16);
+        }
+
+        if(joypad() == J_DOWN && y < 120)
+        {
+            y++;
+            y++;
+            move_sprite(0, x, y);
+            move_sprite(1, x, y+16);
+     
+        }
+        delay(15);
+    }
+}
