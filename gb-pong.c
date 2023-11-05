2333983cec6a99b1657f3d35c1f0d9eccbc16f60
AMGirouxMtA <113360644+AlexMGiroux@users.noreply.github.com>
2023-11-05 18:52:45 -0400

    Refactored some and added comments

--- gb-pong.c
+++ gb-pong.c
@@ -4,26 +4,32 @@
 #include "sprites.c"
 
 void updateScore(int yourScore, int oppScore);
+void bounceSound();
+void fiveDingsSound();
 
 void main()
 {    
-    NR52_REG = 0x80;
-    NR50_REG = 0x77;
-    NR51_REG = 0xFF;
+    NR52_REG = 0x80; // Audio master control - Audio on
+    NR50_REG = 0x77; // Master volume & VIN panning - set left and right channel to max volume
+    NR51_REG = 0xFF; // Sound panning - all channels, left and right
     
     int yourScore = 0;
     int oppScore = 0;
     updateScore(yourScore, oppScore);
 
+    // Intial direction of ball
     bool ballRight = TRUE;
     bool ballUp = TRUE;
     
+    // Initial position of player paddle (left)
     int x = 12;
     int y = 75;
 
+    // Initial position of opponent paddle (right)
     int oppx = 156;
     int oppy = 75;
 
+    // Initial position of ball
     int ballx = 85;
     int bally = 75;
 
@@ -46,8 +52,10 @@ void main()
     move_sprite(4, ballx, bally);
     SHOW_SPRITES;
 
+    // Main game Loop
     while(1)
     {
+        // Move ball according to current direction
         if (ballRight)
         {
             ballx++;
@@ -70,18 +78,17 @@ void main()
             move_sprite(4, ballx, bally);
         }
 
+        // Ball crosses opponent paddle x-position going right
         if(ballx > oppx - 6)
         {
+            // Bounces if aligned with part of the paddle
             if(bally < oppy + 30 && bally > oppy - 8)
             {
                 ballRight = FALSE;
-
-                NR10_REG = 0x7E;
-                NR11_REG = 0xC1;
-                NR12_REG = 0x66;
-                NR13_REG = 0x7C;
-                NR14_REG = 0xC6;
+                bounceSound();
             }
+
+            // Otherwise scores
             else
             {
                 ballx = 85;
@@ -89,31 +96,22 @@ void main()
                 yourScore++;
                 ballRight = !ballRight;
 
-                int i;
-                for(i = 0; i < 5; i++)
-                {
-                    NR21_REG = 0x81;
-                    NR22_REG = 0xA6;
-                    NR23_REG = 0x37;
-                    NR24_REG = 0x87;
-                    delay(180);
-                }
-
+                fiveDingsSound();
                 updateScore(yourScore, oppScore);
             }
         }
+
+        // Ball crosses player paddle x-position going left
         else if(ballx < x + 6)
         {
+            // Bounces if aligned with part of the paddle
             if(bally < y + 30 && bally > y - 8)
             {
                 ballRight = TRUE;
-
-                NR10_REG = 0x7E;
-                NR11_REG = 0xC1;
-                NR12_REG = 0x66;
-                NR13_REG = 0x7C;
-                NR14_REG = 0xC6;
+                bounceSound();
             }
+
+            // Otherwise scores
             else
             {
                 ballx = 85;
@@ -121,20 +119,12 @@ void main()
                 oppScore++;
                 ballRight = !ballRight;
 
-                int i;
-                for(i = 0; i < 5; i++)
-                {
-                    NR21_REG = 0x81;
-                    NR22_REG = 0xA6;
-                    NR23_REG = 0x37;
-                    NR24_REG = 0x87;
-                    delay(180);
-                }
-                
+                fiveDingsSound();
                 updateScore(yourScore, oppScore);
             }
         }
 
+        // Ball ricochets off ceiling and floor of screen
         if(bally < 20)
         {
             ballUp = FALSE;
@@ -144,13 +134,14 @@ void main()
             ballUp = TRUE;
         }
 
+        // Opponent paddle chases ball up and down
         if (bally > oppy)
         {
             if (ballx > (95))
             {
                 oppy++;
                 move_sprite(2, oppx, oppy);
-                move_sprite(3, oppx, oppy+16);
+                move_sprite(3, oppx, oppy + 16);
             }
 
             else
@@ -169,6 +160,7 @@ void main()
                 move_sprite(3, oppx, oppy+16);
             }
         }
+
         else if (bally < oppy)
         {
             if (ballx > (95))
@@ -195,6 +187,7 @@ void main()
             }
         }
         
+        // Control of player paddle with UP and DOWN buttons
         if(joypad() == J_UP && y > 20)
         {
             y--;
@@ -211,6 +204,7 @@ void main()
             move_sprite(1, x, y+16);
      
         }
+        
         delay(15);
     }
 }
@@ -219,4 +213,25 @@ void updateScore(int yourScore, int oppScore)
 {
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf(" %d               %d\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", yourScore, oppScore);
+}
+
+void bounceSound()
+{
+    NR10_REG = 0x7E;
+    NR11_REG = 0xC1;
+    NR12_REG = 0x66;
+    NR13_REG = 0x7C;
+    NR14_REG = 0xC6;
+}
+
+void fiveDingsSound()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        NR21_REG = 0x81;
+        NR22_REG = 0xA6;
+        NR23_REG = 0x37;
+        NR24_REG = 0x87;
+        delay(180);
+    }
 }
